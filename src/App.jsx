b3c33989196efc7ca9ab96e90e154fdfbbb5ab1a
avishek195 <avishekmistry420@gmail.com>
2024-02-10 00:43:30 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { Route, Routes, Link } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Header from "./Navbar/Header.jsx";
+import About from "./components/About.jsx";
 
-const Nav = () => {
-  return (
-    <>
-      <nav>
-        <Link to="" />
-      </nav>
-    </>
-  );
-};
-
+import "./App.css";
 const App = () => {
-  return <></>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
