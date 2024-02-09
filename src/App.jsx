737d665e7aa7573cb958741e97a5859b2939af25
avishek195 <avishekmistry420@gmail.com>
2024-02-09 23:08:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes, Link } from "react";
+
+const Nav = () => {
+  return (
+    <>
+      <nav>
+        <Link to="" />
+      </nav>
+    </>
+  );
+};
+
+const App = () => {
+  return <></>;
+};
+
+export default App;
